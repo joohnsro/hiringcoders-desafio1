9d838a8565d029ebaf41f309f309ca6da90f3b79
joohnsro <jonathansroliveira@gmail.com>
2021-07-13 13:46:27 -0300

    Ajustando rolagem de página a partir do anchor.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,4 +7,13 @@
 
         localStorage.setItem('newsletter-email', input.value);
     })
+
+    const rollDown = document.getElementById('roll-down');
+
+    rollDown.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let scrollTo = document.getElementById('reasons');
+        window.scrollTo(0, scrollTo.scrollHeight);  
+    })
 })();
